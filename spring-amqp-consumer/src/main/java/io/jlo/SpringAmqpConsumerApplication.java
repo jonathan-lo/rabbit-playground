4856fe32190e1b74727fdfd70bc53db9cfef6401
Jonathan Lo <jchl027@gmail.com>
2017-09-26 22:07:02 +1000

    initial

---
+++ spring-amqp-consumer/src/main/java/io/jlo/SpringAmqpConsumerApplication.java
@@ -0,0 +1,12 @@
+package io.jlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAmqpConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAmqpConsumerApplication.class, args);
+	}
+}
